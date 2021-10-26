fb6818197d85ecf0f8ecef5c974e91f0f279ab31
Benjamin Rubin <brubin13@umd.edu>
2021-10-26 12:43:27 -0400

    Allow removal of enemy peices in a mill if they only have peices in mills

--- ninemensmorris.py
+++ ninemensmorris.py
@@ -24,7 +24,7 @@ class NineMensMorris:
         self.black_phase = 1
 
     def isMill(self, prev, new):
-            return False
+            return True
     
     def move(self, prev, new, remove):
             return self
@@ -32,34 +32,20 @@ class NineMensMorris:
     def getValidMoves(self, current_player):
             # Find out what phase the current player is in
             if (current_player == 1):
-                if (self.white_pieces_avail > 0):
-                    phase = 1
-                elif (self.white_pieces_on_board == 3):
-                    phase = 3
-                else:
-                    phase = 2
+                phase = self.white_phase
             else:
-                if (self.black_pieces_avail > 0):
-                    phase = 1
-                elif (self.black_pieces_on_board == 3):
-                    phase = 3
-                else:
-                    phase = 2
+                phase = self.black_phase
 
             # Finding all possible moves
-            valid_moves = np.empty(0, type(self)) # Array of valid states
+            valid_moves = [] # Array of valid states
             if (phase == 1): # Placing One
                 for i in range(3):
                     for j in range(8):
                         if (self.board[i][j] == 0): # Empty space
                             if (self.isMill(None, (i, j))): # Check if placing will cause a mill
-                                for ii in range(3):
-                                    for jj in range(8):
-                                        if (current_player % 2 + 1 == self.board[ii][jj] and not self.ismill(None, (ii, jj))):
-                                            # If the peice is the other players and not part of a mill remove it
-                                            valid_moves = np.append(valid_moves, self.move((None, (i, j), (ii, jj))))
+                                self.expandMillMove(None, (i, j), current_player, valid_moves)              
                             else:
-                                valid_moves = np.append(valid_moves, self.move(None, (i, j), None)) # Add non-mill state
+                                valid_moves.append(self.move(None, (i, j), None)) # Add non-mill state
             elif(phase == 2): # Sliding Phase
                 print("Unimplemented")
             else: # Jumpinh phase
@@ -69,13 +55,28 @@ class NineMensMorris:
                             for ii in range(3):
                                 for jj in range(8):
                                     if (self.board[ii][jj] == 0): # Empty space
-                                        if (self.isMill((i, j), (i, j))): # Check if moving there creates a mill
-                                            for iii in range(3):
-                                                for jjj in range(8):
-                                                    if (current_player % 2 + 1 == self.board[iii][jjj] and not self.ismill(None, (iii, jjj))): # Move and renove peice if it isnt part of a mill
-                                                        valid_moves = np.append(valid_moves, self.move(((i, j), (ii, jj), (iii, jjj))))
+                                        if (self.isMill((i, j), (ii, jj))): # Check if moving there creates a mill
+                                            self.expandMillMove((i, j), (ii, jj), current_player, valid_moves)
                                         else:
-                                            valid_moves = np.append(valid_moves, self.move((i, j), (ii, jj), None)) # Add non-mill state
+                                            valid_moves.append(self.move((i, j), (ii, jj), None)) # Add non-mill state
                                             
             return valid_moves # Return the possible moves
+   
+    # Helper function that expands moves that require a player to remove an enemy peice 
+    def expandMillMove(self, prev, new, current_player, valid_moves):
+        opponent_peices_in_mill = []
+        found_non_mill = False
+        for i in range(3):
+            for j in range(8):
+                if (self.board[i][j] == current_player % 2 + 1):
+                    if (not self.isMill(None, (i, j))):
+                        # If the peice is the other players and not part of a mill remove it
+                        valid_moves.append(self.move(prev, new, (i, j)))
+                        found_non_mill = True
+                    elif (not found_non_mill):
+                        opponent_peices_in_mill.append((i, j))
+        if (not found_non_mill):
+            for m in opponent_peices_in_mill:
+                valid_moves.append(self.move(prev, new, (m[0], m[1])))
+
 
