63ba620f6dff914a133665398c3f1f9042511c97
Benjamin Rubin <brubin13@umd.edu>
2021-10-26 22:08:06 -0400

    Updated getValidMoves to work with new move, and minor bug fixes

--- ninemensmorris.py
+++ ninemensmorris.py
@@ -1,7 +1,6 @@
 # Nine Men's Morris Game
 
 import numpy as np
-from numpy.lib.function_base import blackman
 
 class NineMensMorris:
     # Constructor for the game: creates a new game with an empty board and 9 pieces for each player
@@ -29,7 +28,7 @@ class NineMensMorris:
     
     def newState(self):
             temp = NineMensMorris()
-            temp.board = self.board
+            temp.board = np.copy(self.board)
             temp.white_pieces_on_board = self.white_pieces_on_board
             temp.black_pieces_on_board = self.black_pieces_on_board
             temp.white_pieces_avail = self.white_pieces_avail
@@ -39,7 +38,7 @@ class NineMensMorris:
             return temp
     
     def move(self, prev, new, remove, player):
-            temp = self.newState
+            temp = self.newState()
             #player is placing down a piece removing 1 from available pieces
             if prev == None:
                 if player == 1:
@@ -48,11 +47,25 @@ class NineMensMorris:
                     #If there are no more available pieces set phase to sliding
                     if temp.white_pieces_avail == 0:
                         temp.white_phase = self.white_phase + 1
+                    #remove piece if remove isn't none
+                    if remove != None:
+                        temp.board[remove[0]][remove[1]] = 0
+                        temp.black_pieces_on_board = temp.black_pieces_on_board - 1
+                        #if black only has 3 pieces left set it to jump phase
+                        if temp.black_pieces_on_board == 3 and temp.black_pieces_avail == 0:
+                            temp.black_phase = 3
                 else:
                     temp.board[new[0]][new[1]] = 2
                     temp.black_pieces_avail = self.black_pieces_avail - 1
                     if temp.black_pieces_avail == 0:
                         temp.black_phase = self.black_phase + 1
+                    #remove piece if remove isn't none
+                    if remove != None:
+                        temp.board[remove[0]][remove[1]] = 0
+                        temp.white_pieces_on_board = temp.white_pieces_on_board - 1
+                        #if white only has 3 pieces left set it to jump phase
+                        if temp.white_pieces_on_board == 3 and temp.white_pieces_avail == 0:
+                            temp.white_phase = 3
             else:
                 #either a slide or a jump, both cases set prev position to 0 and new 
                 #to 1 or 2 depending on which player is making the move
@@ -93,7 +106,7 @@ class NineMensMorris:
                             if (self.isMill(None, (i, j))): # Check if placing will cause a mill
                                 self.expandMillMove(None, (i, j), current_player, valid_moves)              
                             else:
-                                valid_moves.append(self.move(None, (i, j), None)) # Add non-mill state
+                                valid_moves.append(self.move(None, (i, j), None, current_player)) # Add non-mill state
             elif(phase == 2): # Sliding Phase
                 for i in range(3):
                     for j in range(8):
@@ -106,7 +119,7 @@ class NineMensMorris:
                                 if (self.isMill((i, j), new)): # check if moving there creates a mill
                                     self.expandMillMove((i, j), new, current_player, valid_moves)
                                 else:
-                                    valid_moves.append(self.move((i, j), new, None)) # Add non-mill state
+                                    valid_moves.append(self.move((i, j), new, None, current_player)) # Add non-mill state
             else: # Jumping phase
                 for i in range(3):
                     for j in range(8):
@@ -117,7 +130,7 @@ class NineMensMorris:
                                         if (self.isMill((i, j), (ii, jj))): # Check if moving there creates a mill
                                             self.expandMillMove((i, j), (ii, jj), current_player, valid_moves)
                                         else:
-                                            valid_moves.append(self.move((i, j), (ii, jj), None)) # Add non-mill state
+                                            valid_moves.append(self.move((i, j), (ii, jj), None, current_player)) # Add non-mill state
                                             
             return valid_moves # Return the possible moves
 
@@ -196,7 +209,7 @@ class NineMensMorris:
                 if (self.board[i][j] == current_player % 2 + 1):
                     if (not self.isMill(None, (i, j))):
                         # If the peice is the other players and not part of a mill remove it
-                        valid_moves.append(self.move(prev, new, (i, j)))
+                        valid_moves.append(self.move(prev, new, (i, j), current_player))
                         found_non_mill = True
                     elif (not found_non_mill):
                         # If we have not found a peice outside of a mill, put keep track of peices in mills
@@ -204,6 +217,6 @@ class NineMensMorris:
         if (not found_non_mill):
             for m in opponent_peices_in_mill:
                 # If there are only peices that are part of a mill, make moves that remove them
-                valid_moves.append(self.move(prev, new, (m[0], m[1])))
+                valid_moves.append(self.move(prev, new, (m[0], m[1]), current_player))
 
 
